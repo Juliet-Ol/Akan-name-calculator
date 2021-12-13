d8ba0068752a096f62aa7c2593edc2c879f13dbb
Juliet-Ol <agutjuliet@gmail.com>
2021-12-13 22:57:10 +0300

    add variables

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+function check_akanname(day) {
+   var akanname = document.getElementById("dateOfBirth").value;
+   var akanname = document.getElementById("gender").value;
+
+}
+
+function gender()
+var male = document.getElementById("male").check;
+var female = document.getElementById("female").check;
+if (male) {
+    console.log(male)
+}
+else if (female) {
+    console.log(female)
+}
+
+
+male = maleAkanName ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+maleAkanName = day ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+
+var maleAkanName = days();
+
+
+
+function newFunction() {
+    return ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+}
+
+
+
+
+
+console.log("male")
+
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+
+var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var maleAkanName = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
