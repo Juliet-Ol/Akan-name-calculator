638cdcdf7fddec4467b6ccd08e1b8be04ee8c7ef
Juliet-Ol <agutjuliet@gmail.com>
2021-12-14 13:47:49 +0300

    modify date to day

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function check_akanname(day) {
-   var akanname = document.getElementById("dateOfBirth").value;
+   var akanname = document.getElementById("year").value;
+   var akanname = document.getElementById("month").value; 
+   var akanname = document.getElementById("day").value; 
    var akanname = document.getElementById("gender").value;
 
 }
@@ -15,28 +17,87 @@ else if (female) {
 }
 
 
-male = maleAkanName ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+function getName() {
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+    var maleAkanNames = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var year = parseInt(document.getElementById(yearBorn).value)
+    var month = parseInt(document.getElementById(montBorn))
+    var day = parseInt(document.getElementById(dayBorn))
+    var dayOfTheWeek = ( ( ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7);
+    var akanDay = days[day.toFixed()];
+    var gender = parseInt(document.getElementById("gender").value);
 
-maleAkanName = day ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+    if (gender == male) {
+        if (akanDay==="Sunday"){
+            document.getElementById("output").value = "born on sunday: "+ akanMale[0];
+            return true;
+        }
+        else if (akanDay === "Monday")  {
+            document.getElementById("output").value = "born on monday: "+ akanMale[1];
+            return true;
+        }
 
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+         else if (akanDay === "Tuesday")  {
+            document.getElementById("output").value = "born on tuesday: "+ akanMale[2];
+            return true;    
+         }
 
-var maleAkanName = days();
+            else if (akanDay === "Wednesday")  {
+                document.getElementById("output").value = "born on wednesday: "+ akanMale[3];
+                return true;
+            }    
+                else if (akanDay === "Thursday")  {
+                    document.getElementById("output").value = "born on thursday: "+ akanMale[4];
+                    return true; 
+                }       
+                    else if (akanDay === "Friday")  {
+                        document.getElementById("output").value = "born on friday: "+ akanMale[5];
+                        return true;
+                    }    
+                        else if (akanDay === "Monday")  {
+                            document.getElementById("output").value = "born on monday: "+ akanMale[6];
+                            return true;
 
+    if (gender == female) {
+        if (akanDay==="Sunday") {
+            document.getElementById("output").value = "born on sunday: "+ akanfemale[0];
+            return true;   
+    } 
+    
+        else if (akanDay==="Monday"){
+                document.getElementById("output").value = "born on monday: "+ akanfemale[1];
+                return true;
+        }    
+            else if (akanDay==="Tuesday"){
+                document.getElementById("output").value = "born on tuesday: "+ akanfemale[2];
+                return true; 
+            } 
 
+            else if (akanDay==="Wednesday"){
+                document.getElementById("output").value = "born on wednesday: "+ akanfemale[3];
+                return true; 
+            }        
+             else if (akanDay==="Thursday"){
+                document.getElementById("output").value = "born on thursday: "+ akanfemale[4];
+                return true; 
+            }                        
+            else if (akanDay==="Monday"){
+                document.getElementById("output").value = "born on monday: "+ akanfemale[5];
+                return true;   
+            } 
+             else if (akanDay==="Monday"){
+                document.getElementById("output").value = "born on monday: "+ akanfemale[6];
+                return true;                                                
+        }        
 
-function newFunction() {
-    return ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    
+    }
 }
 
 
 
-
-
-console.log("male")
-
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
-
-var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-var maleAkanName = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+function getName() {
+    document.querySelector (".Akan name").style.backgroundColor = "grey"
+    
+}
